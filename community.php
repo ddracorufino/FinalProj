5a3e850fc5d018f08dceb8c5601f73d6c4058801
ddracorufino <andreiirufinoo@gmail.com>
2025-07-25 23:25:30 +0800

    Update community.php

--- community.php
+++ community.php
@@ -1,6 +1,21 @@
-<?php include('includes/header.php'); ?>
+<?php include('header.php'); ?>
 <main>
     <h2>Stray Community Map</h2>
-    <iframe src="https://www.google.com/maps/d/embed?mid=YOUR_CUSTOM_MAP_ID" width="100%" height="480"></iframe>
+    <p style="text-align: center; margin-bottom: 2rem; color: #666;">
+        Use our interactive map to report stray animals in your area or find nearby rescue centers.
+    </p>
+    
+    <div class="map-container">
+        <iframe src="https://www.google.com/maps/d/embed?mid=YOUR_CUSTOM_MAP_ID" 
+                width="100%" height="480" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    </div>
+    
+    <div style="text-align: center; margin-top: 2rem;">
+        <a href="#" class="btn">Report a Stray Animal</a>
+    </div>
 </main>
-<?php include('includes/footer.php'); ?>
+<?php include('footer.php'); ?>
